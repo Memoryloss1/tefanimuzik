e2cc57e1c969620aa89347527918fb9ccb923fba
Şehmus <67182830+Memoryloss1@users.noreply.github.com>
2023-03-04 21:01:29 +0300

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -52,7 +52,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=f"☀ sʜɪᴢᴜᴋᴀ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ☀",
+                text=f"☀ Grubumuz ☀",
                 url=f"https://t.me/tefanikitap",
             )
         ],
